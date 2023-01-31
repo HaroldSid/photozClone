9383e7cc7b22ccce97952cb32f61239e40e653a5
Harold Delacroix <harold.delacroix@gmail.com>
2023-01-31 17:54:31 +0100

    Initial commit

---
+++ src/test/java/com/springboot/photozClone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.photozClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
